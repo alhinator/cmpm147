c163adc585777bf99fff1f9129ae03addfb0c213
alhinator <alex@leghart.org>
2024-04-06 11:49:21 -0700

    copied html and js from glitch

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -1,34 +1,56 @@
-// project.js - purpose and description here
-// Author: Your Name
-// Date:
+// project.js - this is the generator for the sinister potion recipe generator
+// Author: Alex Leghart
+// Date: 4-7-24
 
-// NOTE: This is how we might start a basic JavaaScript OOP project
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
-  
-  // define a method
-  myMethod() {
-    // code to run when method is called
+const fillers = {
+  name: ["Wizard", "Witch", "Warlock", "Alchemist", "Gambler", "Sniler", "Executioner", "Gay Frog", "President", "Wes Mode", "Gamer"],
+  tasteful: ["Tasteful", "Delicious", "Interesting", "Abhorrent", "Dégoûtant", 'Unusual', "Sinister", "Queer", "Addictive", "Righteous", "Frightening", "Perfected", "Ascendant", "Living", "Dreadful", "Non-alcoholic", "Rainbow", "Starry"],
+  drinknoun: ["Potion", "Brew", "Concotion", "Sipping Drink", "Mixed Drink", "Creation", "Jus"],
+  base_amt: ["a pint", "one shot", "two shots", "eight ounces", "a large jug", "half a glass"],
+  base: ["lemonade", "iced tea", "seltzer water", "ginger beer", "limeade", "naked juice", "apple juice", "orange juice", "cranberry juice", "grapefruit juice", "milk", "cola", "pilk", "sprite", "nyquil", "lean", "kombucha", "coffee", "chai", "matcha", "redbull", "sugar-free monster", "beef broth", "margarita mixer", "grenadine syrup", "g-fuel"],
+  alc: ["vodka", "everclear", "moonshine", "white rum", "coconut rum", "fireball", "cheap whiskey", "expensive whiskey", "brandy", "ale", "mead", "tequila", "white wine", "red wine", "coor's banquet", "miller high life", "banana liqueur"],
+  method_adverb:["Slowly", "Quickly", "Carefully", "Lovingly", "Violently"],
+  method:["stir", "shake", "microwave", "blend", "heat to a rolling boil"],
+  time:["ten seconds", "fifteen seconds", "thirty seconds", "one minute"],
+  garnish: ["a sprig of mint", "salt", "cayenne powder", "tajin", "a dash of lime", "a dash of lemon", "a wedge of lime", "a wedge of orange", "a wedge of lemon", "a sprig of cilantro", "three drops of your own blood", "the tears of a maiden", "MDMA"],
+  served: ["warm", "chilled", "frozen", "piping hot", "lukewarm", "on the rocks"],
+  container:["old fashioned glass", "shotglass", "tall shotglass", "potion bottle", "copper mug", "pint glass", "wooden pint", "coffee mug", "bong", "hydroflask", "dog bowl"]
+};
+
+const template = `Recipe for "$name\'s $tasteful $drinknoun":
+
+Begin by filling a large mixing container with $base_amt of $base.
+Add $base_amt of $alc, and if you're feeling brave, add $base_amt of $alc.
+$method_adverb $method for $time; Garnish with $garnish and serve $served in a $container.`
+;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  box.innerText = story;
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+clicker.onclick = generate;
+
+generate();
